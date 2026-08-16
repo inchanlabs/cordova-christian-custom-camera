e7aace18c816ab9db149e724bd0933c00dd72346
inchanlabs <christian.labs024@gmail.com>
2026-08-16 18:10:08 +0800

    Update Camera.js
    update camera.js

--- www/Camera.js
+++ www/Camera.js
@@ -1,1 +1,20 @@
 
+var exec = require("cordova/exec");
+
+var Camera = {
+
+    getPicture: function (successCallback, errorCallback, options) {
+
+        options = options || {};
+
+        exec(
+            successCallback,
+            errorCallback,
+            "CustomCamera",
+            "takePicture",
+            [options]
+        );
+    }
+};
+
+module.exports = Camera;
