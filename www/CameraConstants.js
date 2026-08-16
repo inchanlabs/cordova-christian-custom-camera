99bb26f32c75c80a406cf9f69e2d5bbb13964fd5
inchanlabs <christian.labs024@gmail.com>
2026-08-16 18:09:32 +0800

    Update CameraConstants.js
    add camera constraints.

--- www/CameraConstants.js
+++ www/CameraConstants.js
@@ -1,1 +1,29 @@
 
+var Camera = {
+    DestinationType: {
+        DATA_URL: 0,
+        FILE_URI: 1
+    },
+
+    EncodingType: {
+        JPEG: 0,
+        PNG: 1
+    },
+
+    MediaType: {
+        PICTURE: 0
+    },
+
+    PictureSourceType: {
+        PHOTOLIBRARY: 0,
+        CAMERA: 1,
+        SAVEDPHOTOALBUM: 2
+    },
+
+    CameraDirection: {
+        BACK: 0,
+        FRONT: 1
+    }
+};
+
+module.exports = Camera;
