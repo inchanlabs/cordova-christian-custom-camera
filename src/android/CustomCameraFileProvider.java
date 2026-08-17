ad32f452cc5081ce1b6084e40d65fca85f684d33
inchanlabs <christian.labs024@gmail.com>
2026-08-17 13:19:40 +0800

    Update CustomCameraFileProvider.java

--- src/android/CustomCameraFileProvider.java
+++ src/android/CustomCameraFileProvider.java
@@ -3,5 +3,4 @@ package com.christian.customcamera;
 import androidx.core.content.FileProvider;
 
 public class CustomCameraFileProvider extends FileProvider {
-    // Custom FileProvider implementation
 }
