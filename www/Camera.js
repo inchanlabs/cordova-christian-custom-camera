4dcc394349f02f97b99868dbdbaf215dbd140107
inchanlabs <christian.labs024@gmail.com>
2026-08-17 19:25:21 +0800

    Update Camera.js

--- www/Camera.js
+++ www/Camera.js
@@ -3,15 +3,29 @@ var exec = require("cordova/exec");
 var ChristianCustomCamera = {
     getPicture: function (successCallback, errorCallback, options) {
         var opts = options || {};
+
+        console.log("[ChristianCustomCamera] getPicture called");
+        console.log("[ChristianCustomCamera] options:", opts);
+
         exec(
             function(result) {
+                console.log("[ChristianCustomCamera] SUCCESS callback received");
+                console.log("[ChristianCustomCamera] result:", result);
+
                 if (typeof successCallback === "function") {
                     successCallback(result);
+                } else {
+                    console.warn("[ChristianCustomCamera] No success callback provided");
                 }
             },
             function(err) {
+                console.error("[ChristianCustomCamera] ERROR callback received");
+                console.error("[ChristianCustomCamera] error:", err);
+
                 if (typeof errorCallback === "function") {
                     errorCallback(err);
+                } else {
+                    console.warn("[ChristianCustomCamera] No error callback provided");
                 }
             },
             "CustomCamera",
