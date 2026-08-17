431673afd3d4d09788e92d3eef5991b02827f640
inchanlabs <christian.labs024@gmail.com>
2026-08-17 13:57:19 +0800

    Update CameraLauncher.kt

--- src/android/CameraLauncher.kt
+++ src/android/CameraLauncher.kt
@@ -1,22 +1,30 @@
 package com.christian.customcamera
 
 import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaInterface
 import org.apache.cordova.CordovaPlugin
+import org.apache.cordova.CordovaWebView
+import org.apache.cordova.PluginResult
 import org.json.JSONArray
 
 class CameraLauncher : CordovaPlugin() {
+
+    override fun initialize(cordova: CordovaInterface, webView: CordovaWebView) {
+        super.initialize(cordova, webView)
+    }
 
     override fun execute(
         action: String,
         args: JSONArray,
         callbackContext: CallbackContext
     ): Boolean {
-        if (action == "takePicture") {
-            callbackContext.success("NATIVE BRIDGE IS WORKING!")
+        if ("takePicture" == action) {
+            val result = PluginResult(PluginResult.Status.OK, "NATIVE BRIDGE IS WORKING!")
+            callbackContext.sendPluginResult(result)
             return true
         }
 
-        callbackContext.error("Invalid action: $action")
+        callbackContext.error("Action not recognized: $action")
         return false
     }
 }
