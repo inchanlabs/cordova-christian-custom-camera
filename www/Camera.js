eac0d06cfab028e8110ffc937675cf561bfb197c
inchanlabs <christian.labs024@gmail.com>
2026-08-17 11:46:00 +0800

    Update Camera.js

--- www/Camera.js
+++ www/Camera.js
@@ -11,54 +11,13 @@ var Camera = {
 
     getPicture: function (successCallback, errorCallback, options) {
 
-        options = options || {};
-
-        console.log("CUSTOM CAMERA: BEFORE EXEC");
-
-        try {
-
-            exec(
-                function (result) {
-
-                    console.log(
-                        "CUSTOM CAMERA: EXEC SUCCESS: " + result
-                    );
-
-                    if (successCallback) {
-                        successCallback(result);
-                    }
-                },
-
-                function (error) {
-
-                    console.log(
-                        "CUSTOM CAMERA: EXEC ERROR: " + error
-                    );
-
-                    if (errorCallback) {
-                        errorCallback(error);
-                    }
-                },
-
-                "CustomCamera",
-                "takePicture",
-                [options]
-            );
-
-            console.log("CUSTOM CAMERA: AFTER EXEC");
-
-        } catch (error) {
-
-            console.log(
-                "CUSTOM CAMERA: EXEC EXCEPTION: " + error
-            );
-
-            if (errorCallback) {
-                errorCallback(
-                    "EXEC EXCEPTION: " + error
-                );
-            }
-        }
+        exec(
+            successCallback,
+            errorCallback,
+            "CustomCamera",
+            "takePicture",
+            []
+        );
     }
 };
 
